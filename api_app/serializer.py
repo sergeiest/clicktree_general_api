4251a58bef32838f51a7593d867a2d13dc3e8ce6
tomaszb <tbacewicz@gmail.com>
2014-04-11 15:09:11 -0700

    fixed bug in request posts if headers don't exist in data

--- api_app/serializer.py
+++ api_app/serializer.py
@@ -12,15 +12,17 @@ class CustomJSONSerializer(Serializer):
         if 'time' in data:
                 data['time'] = datetime.fromtimestamp(float(data['time'])/1000.0, tz=pytz.utc)
         logging.info("after loading time to data in from_json")
-        if 'host' in data['headers']:
-                data['host'] = data['headers']['host'] if len(data['headers']['host']) < 200 else data['headers']['host'][:200]
-        if 'useragent' in data['headers']:
-                data['useragent'] = data['headers']['useragent'] if len(data['headers']['useragent']) < 200 else data['headers']['useragent'][:200]
-        if 'language' in data['headers']:
-                data['lang'] = data['headers']['language']
-        if 'referer' in data['headers']:
-                data['referer'] = data['headers']['referer'] if len(data['headers']['referer']) < 200 else data['headers']['referer'][:200]
-        if 'encoding' in data['headers']:
-                data['encode'] = data['headers']['encoding']
+        if 'headers' in data:
+            if 'host' in data['headers']:
+                    data['host'] = data['headers']['host'] if len(data['headers']['host']) < 200 else data['headers']['host'][:200]
+            if 'useragent' in data['headers']:
+                    data['useragent'] = data['headers']['useragent'] if len(data['headers']['useragent']) < 200 else data['headers']['useragent'][:200]
+            if 'language' in data['headers']:
+                    data['lang'] = data['headers']['language']
+            if 'referer' in data['headers']:
+                    data['referer'] = data['headers']['referer'] if len(data['headers']['referer']) < 200 else data['headers']['referer'][:200]
+            if 'encoding' in data['headers']:
+                    data['encode'] = data['headers']['encoding']
        # logging.info("finish from_json")
+
         return data
